2a62400453f752003f853d0099f7ac7acb8ec57c
WithyouTt <39683803+jimmg5202@users.noreply.github.com>
2024-11-22 15:58:55 +0800

    Update __init__.py

--- plugins/chinesesubfinder/__init__.py
+++ plugins/chinesesubfinder/__init__.py
@@ -20,7 +20,7 @@ class ChineseSubFinder(_PluginBase):
     # 插件图标
     plugin_icon = "chinesesubfinder.png"
     # 插件版本
-    plugin_version = "1.1"
+    plugin_version = "1.2"
     # 插件作者
     plugin_author = "jxxghp"
     # 作者主页
