16b9caa5ca12aa9095bfff867d1364d5e606784d
WithyouTt <39683803+jimmg5202@users.noreply.github.com>
2024-11-23 10:02:59 +0800

    Update __init__.py

--- plugins/strmcsf/__init__.py
+++ plugins/strmcsf/__init__.py
@@ -15,7 +15,7 @@ from app.utils.http import RequestUtils
 
 class strmcsf1(_PluginBase):
     # 插件名称
-    plugin_name = "strmcsf1"
+    plugin_name = "strmcsf"
     # 插件描述
     plugin_desc = "整理入库时通知ChineseSubFinder下载字幕。"
     # 插件图标
