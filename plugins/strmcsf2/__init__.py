1328d70b170dce845294e6cb897c7027a17b8597
WithyouTt <39683803+jimmg5202@users.noreply.github.com>
2024-11-23 17:04:50 +0800

    Update __init__.py

--- plugins/strmcsf2/__init__.py
+++ plugins/strmcsf2/__init__.py
@@ -20,7 +20,7 @@ class strmcsf2(_PluginBase):
     # 插件图标
     plugin_icon = "chinesesubfinder.png"
     # 插件版本
-    plugin_version = "3.3"
+    plugin_version = "3.4"
     # 插件作者
     plugin_author = "jxxghp"
     # 作者主页
